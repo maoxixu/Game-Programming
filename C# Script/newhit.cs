1960db95b84cc4f40f4f47d4b61e1160c78b96ce
maoxixu <145374325+maoxixu@users.noreply.github.com>
2023-12-11 11:36:46 -0500

    Update newhit.cs

--- C# Script/newhit.cs
+++ C# Script/newhit.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class newhit : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    // ...
 
     void OnCollisionEnter(Collision other)
     {
+        // Check if the colliding object has the "Player" tag
         if (other.gameObject.tag == "Player")
         {
-
-
+            // Change the color to green when hit by the player
             GetComponent<MeshRenderer>().material.color = Color.green;
+            // Change the tag of the object to "Hit"
             gameObject.tag = "Hit";
-
-
         }
     }
 
-
-
+  
 }
