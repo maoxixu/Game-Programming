07692441fd3ffe814fe9b0f4518d131f30472772
maoxixu <145374325+maoxixu@users.noreply.github.com>
2023-12-11 11:35:48 -0500

    Update cylinder.cs

--- C# Script/cylinder.cs
+++ C# Script/cylinder.cs
@@ -7,12 +7,13 @@ public class cylinder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialization code can be placed here
     }
 
     // Update is called once per frame
     void Update()
-    {
-        transform.Rotate(200*Time.deltaTime, 0, 0);
+    {    
+        // Rotate the object around the X-axis at a speed of 200 degrees per second
+        transform.Rotate(200 * Time.deltaTime, 0, 0);
     }
 }
