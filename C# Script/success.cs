7468e09f3271331bd9c8eb623075144e4759271b
maoxixu <145374325+maoxixu@users.noreply.github.com>
2023-12-11 11:37:32 -0500

    Update success.cs

--- C# Script/success.cs
+++ C# Script/success.cs
@@ -6,19 +6,17 @@ using UnityEngine.SceneManagement;
 public class success : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    // ...
 
     void OnCollisionEnter(Collision other)
     {
+        // Check if the colliding object has the "Player" tag
         if (other.gameObject.tag == "Player")
         {
-
-
+            // Load the "success" scene when the player collides with this object
             SceneManager.LoadScene("success");
-
         }
     }
 
-
-
+   
 }
